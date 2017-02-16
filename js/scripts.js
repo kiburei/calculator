602cac5aa3e0fc7c8d11628f37c59ef0e2c95c33
kiburei <akiburei@gmail.com>
2017-02-16 15:58:12 +0300

    js adition calculator

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+}
+ var number1 = parseInt(prompt("Enter Number:"));
+ var number2 = parseInt(prompt("ENter second Number:"));
+
+ alert(add(number1, number2));
